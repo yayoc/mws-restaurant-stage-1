7f7d6698df1b7d983993ba11c88e31848ddb9c2c
yayoc <n.yayoshi+github@gmail.com>
2019-05-19 16:19:27 +0900

    Replace MAPBOX Key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoieWF5b2MiLCJhIjoiY2p2bnh5MzE4MWdkazRjb2Y0ZGFvbWlpYSJ9.lnEXWrvLUCTbkRMHc0_XtQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
